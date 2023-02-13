c6a80049f8c21dad16531380868c7a52a2d84ae3
Gordonby <gordon.byers@microsoft.com>
2023-02-13 22:11:49 +0000

    choco
    Signed-off-by: Gordonby <gordon.byers@microsoft.com>

--- imageBuilderScripts/choco-azure.ps1
+++ imageBuilderScripts/choco-azure.ps1
@@ -6,16 +6,16 @@ choco install azcopy10 -y
 choco install azure-cli -y
 choco install azure-iot-installer -y
 choco install docker-desktop -y
-choco install Firefox
-choco install flux
-choco install gh
-choco install git
-choco install github-desktop
-choco install GoogleChrome
-choco install jq
+choco install Firefox -y
+choco install flux -y
+choco install gh -y
+choco install git -y
+choco install github-desktop -y
+choco install GoogleChrome -y
+choco install jq -y
 choco install kubernetes-cli -y
 choco install kubernetes-helm -y
-choco install paint.net
+choco install paint.net -y
 choco install pulumi -y
 choco install pwsh -y
 choco install servicebusexplorer -y
